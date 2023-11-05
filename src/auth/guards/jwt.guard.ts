46a5136b3b058991715fdac8353ee8ed7a231818
ikhela-04KK <kankoffi36@gmail.com>
2023-11-05 20:36:16 +0000

    succefully implementing guard implementation

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,16 +1,12 @@
-import {CanActivate, ExecutionContext, UnauthorizedException} from '@nestjs/common'; 
+import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from '@nestjs/common'; 
 import { JwtService } from '@nestjs/jwt';
 import  {Request} from 'express';
 import { Observable } from 'rxjs';
 
+@Injectable()
 export class JwtGuard implements CanActivate{
     constructor(private jwtService: JwtService){}
-
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        throw new Error("Method not implemented.");
-    }
-
-    async CanActiavte(context:ExecutionContext):Promise<boolean>{
+    async canActivate(context: ExecutionContext): Promise<boolean>  {
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request); 
         if(!token){
@@ -25,11 +21,13 @@ export class JwtGuard implements CanActivate{
             throw new UnauthorizedException(); 
         }
         return true; 
-    }
+    }    
+
+       
 
     private extractTokenFromHeader(request:Request){
         const [type, token] = request.headers.authorization.split(' ') ?? [];
-        return type == 'Bearer' ? token: undefined; 
+        return type === 'Bearer' ? token: undefined; 
     }
     
 }
