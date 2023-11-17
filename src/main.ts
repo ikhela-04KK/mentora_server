b8cff0289331db0428a36c42fb3bd0d286b49a0e
ikhela-04KK <kankoffi36@gmail.com>
2023-11-17 09:45:22 +0000

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 // pipe de validation intégré à nest js qui fonctionne avec class-validator et class-transformer
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // using of globalPipe to trnsoform data retrieving on prisma database
 
